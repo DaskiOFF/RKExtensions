805c31992597624039056a6b30ebb83c7b09e48c
Roman Kotov <waydeveloper@gmail.com>
2019-06-26 23:40:25 +0300

    Initial Commit

---
+++ RKExtensions/RKExtensions.h
@@ -0,0 +1,19 @@
+//
+//  RKExtensions.h
+//  RKExtensions
+//
+//  Created by Roman Kotov on 26/06/2019.
+//  Copyright © 2019 Roman Kotov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RKExtensions.
+FOUNDATION_EXPORT double RKExtensionsVersionNumber;
+
+//! Project version string for RKExtensions.
+FOUNDATION_EXPORT const unsigned char RKExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RKExtensions/PublicHeader.h>
+
+
